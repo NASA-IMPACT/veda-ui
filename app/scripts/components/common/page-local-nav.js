d5a04aa0b91b13fcb2ad3c8682408aff9df219d3
Ricardo Mestre <ricardo@duplos.org>
2022-02-18 16:27:04 +0000

    Structure local nav breadcrumb

--- app/scripts/components/common/page-local-nav.js
+++ app/scripts/components/common/page-local-nav.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import T from 'prop-types';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { reveal } from '@devseed-ui/animation';
+import { NavLink } from 'react-router-dom';
 
-import { glsp, themeVal, media } from '@devseed-ui/theme-provider';
+import { glsp, themeVal, media, listReset } from '@devseed-ui/theme-provider';
+import { reveal } from '@devseed-ui/animation';
+import { Overline } from '@devseed-ui/typography';
+
 import { variableGlsp } from '../../styles/variable-utils';
+
 import {
   datasetExplorePath,
   datasetOverviewPath,
@@ -18,7 +21,7 @@ const PageLocalNavSelf = styled.nav`
   z-index: 100;
   display: flex;
   flex-flow: row nowrap;
-  align-items: center;
+  align-items: end;
   gap: ${variableGlsp()};
   padding: ${variableGlsp(0.5, 1)};
   background: ${themeVal('color.base-50')};
@@ -26,9 +29,26 @@ const PageLocalNavSelf = styled.nav`
   animation: ${reveal} 0.32s ease 0s 1;
 `;
 
-const LocalTitle = styled.div`
-  a,
-  a:visited {
+const LocalBreadcrumb = styled.ol`
+  ${listReset()}
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  gap: ${glsp(0.5)};
+
+  ${media.largeUp`
+    gap: ${glsp()};
+  `}
+`;
+
+const SectionTitle = styled(Overline).attrs({
+  as: 'a'
+})`
+  color: currentColor;
+  opacity: 0.64;
+
+  &,
+  &:visited {
     color: inherit;
     text-decoration: none;
   }
@@ -49,7 +69,7 @@ const LocalMenu = styled.ul`
   }
 `;
 
-const LocalMenuLink = styled(Link)`
+const LocalMenuLink = styled(NavLink)`
   appearance: none;
   position: relative;
   display: flex;
@@ -109,24 +129,31 @@ function PageLocalNav(props) {
 
   return (
     <PageLocalNavSelf>
-      <LocalTitle>
-        <LocalMenuLink to={datasetOverviewPath(thematic, dataset)}>
-          {title}
-        </LocalMenuLink>
-      </LocalTitle>
+      <LocalBreadcrumb>
+        <li>
+          <SectionTitle href='/' aria-label='Datasets'>
+            Dataset
+          </SectionTitle>
+        </li>
+        <li>
+          <LocalMenuLink to={datasetOverviewPath(thematic, dataset)} as='a'>
+            {title}
+          </LocalMenuLink>
+        </li>
+      </LocalBreadcrumb>
       <LocalMenu>
         <li>
-          <LocalMenuLink to={datasetOverviewPath(thematic, dataset)}>
+          <LocalMenuLink to={datasetOverviewPath(thematic, dataset)} end>
             Overview
           </LocalMenuLink>
         </li>
         <li>
-          <LocalMenuLink to={datasetExplorePath(thematic, dataset)}>
+          <LocalMenuLink to={datasetExplorePath(thematic, dataset)} end>
             Exploration
           </LocalMenuLink>
         </li>
         <li>
-          <LocalMenuLink to={datasetUsagePath(thematic, dataset)}>
+          <LocalMenuLink to={datasetUsagePath(thematic, dataset)} end>
             Usage
           </LocalMenuLink>
         </li>
