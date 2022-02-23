226bb47df4adb2d675131dceb1840aac1b257641
Daniel da Silva <danielfdsilva@gmail.com>
2022-02-23 10:21:02 +0000

    Implement dynamic page local nav

--- app/scripts/components/common/page-local-nav.js
+++ app/scripts/components/common/page-local-nav.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import T from 'prop-types';
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
-
+import { Link, NavLink } from 'react-router-dom';
 import {
   glsp,
   themeVal,
@@ -10,23 +9,20 @@ import {
   listReset,
   truncated
 } from '@devseed-ui/theme-provider';
-
 import {
   CollecticonChevronDownSmall,
   CollecticonChevronUpSmall
 } from '@devseed-ui/collecticons';
-
 import { reveal } from '@devseed-ui/animation';
 import { Overline } from '@devseed-ui/typography';
+import { Dropdown, DropMenu, DropMenuItem } from '@devseed-ui/dropdown';
 
-import { variableGlsp } from '../../styles/variable-utils';
-
+import { variableGlsp } from '$styles/variable-utils';
 import {
   datasetExplorePath,
   datasetOverviewPath,
   datasetUsagePath
-} from '../../utils/routes';
-import { Dropdown, DropMenu, DropMenuItem } from '@devseed-ui/dropdown';
+} from '$utils/routes';
 
 const PageLocalNavSelf = styled.nav`
   position: sticky;
@@ -118,7 +114,7 @@ const LocalMenuLink = styled(NavLink)`
 `;
 
 const SectionParentLink = styled(Overline).attrs({
-  as: 'a'
+  as: Link
 })`
   color: currentColor;
   opacity: 0.64;
@@ -139,66 +135,59 @@ const SectionLink = styled(LocalMenuLink)`
 `;
 
 function PageLocalNav(props) {
-  const { thematic, dataset, title } = props;
+  const { localMenuCmp, parentName, parentLabel, parentTo, items, currentId } =
+    props;
+
+  const currentItem = items.find((o) => o.id === currentId);
 
   return (
     <PageLocalNavSelf>
       <LocalBreadcrumb>
         <li>
-          <SectionParentLink href='/' aria-label='Datasets'>
-            Dataset
+          <SectionParentLink to={parentTo} aria-label={parentLabel}>
+            {parentName}
           </SectionParentLink>
         </li>
         <li>
-          <Dropdown
-            alignment='left'
-            // eslint-disable-next-line no-unused-vars
-            triggerElement={({ active, className, ...rest }) => (
-              <SectionLink {...rest} as='button'>
-                <span>{title}</span>{' '}
-                {active ? (
-                  <CollecticonChevronUpSmall />
-                ) : (
-                  <CollecticonChevronDownSmall />
-                )}
-              </SectionLink>
-            )}
-          >
-            <DropMenu>
-              {[{ id: 't', name: 'Test dataset' }].map((t) => (
-                <li key={t.id}>
-                  <DropMenuItem
-                    as={NavLink}
-                    to={`/${t.id}`}
-                    aria-current={null}
-                    active={t.id === thematic.data.id}
-                    data-dropdown='click.close'
-                  >
-                    {t.name}
-                  </DropMenuItem>
-                </li>
-              ))}
-            </DropMenu>
-          </Dropdown>
+          {items.length > 1 ? (
+            <Dropdown
+              alignment='left'
+              // eslint-disable-next-line no-unused-vars
+              triggerElement={({ active, className, ...rest }) => (
+                <SectionLink {...rest} as='button'>
+                  <span>{currentItem.name}</span>{' '}
+                  {active ? (
+                    <CollecticonChevronUpSmall />
+                  ) : (
+                    <CollecticonChevronDownSmall />
+                  )}
+                </SectionLink>
+              )}
+            >
+              <DropMenu>
+                {items.map((t) => (
+                  <li key={t.id}>
+                    <DropMenuItem
+                      as={NavLink}
+                      to={`${parentTo}/${t.id}`}
+                      aria-current={null}
+                      active={t.id === currentItem.id}
+                      data-dropdown='click.close'
+                    >
+                      {t.name}
+                    </DropMenuItem>
+                  </li>
+                ))}
+              </DropMenu>
+            </Dropdown>
+          ) : (
+            <SectionLink as={Link} to={`${parentTo}/${currentId}`}>
+              <span>{currentItem.name}</span>
+            </SectionLink>
+          )}
         </li>
       </LocalBreadcrumb>
-      <LocalMenu>
-        <li>
-          <LocalMenuLink to={datasetOverviewPath(thematic, dataset)} end>
-            Overview
-          </LocalMenuLink>
-        </li>
-        <li>
-          <LocalMenuLink to={datasetExplorePath(thematic, dataset)} end>
-            Exploration
-          </LocalMenuLink>
-        </li>
-        <li>
-          <LocalMenuLink to={datasetUsagePath(thematic, dataset)} end>
-            Usage
-          </LocalMenuLink>
-        </li>
-      </LocalMenu>
+      {localMenuCmp}
     </PageLocalNavSelf>
   );
 }
@@ -206,7 +195,39 @@ function PageLocalNav(props) {
 export default PageLocalNav;
 
 PageLocalNav.propTypes = {
-  title: T.string,
+  localMenuCmp: T.node,
+  parentName: T.string,
+  parentLabel: T.string,
+  parentTo: T.string,
+  items: T.array,
+  currentId: T.string
+};
+
+export function DatasetsLocalMenu(props) {
+  const { thematic, dataset } = props;
+
+  return (
+    <LocalMenu>
+      <li>
+        <LocalMenuLink to={datasetOverviewPath(thematic, dataset)} end>
+          Overview
+        </LocalMenuLink>
+      </li>
+      <li>
+        <LocalMenuLink to={datasetExplorePath(thematic, dataset)} end>
+          Exploration
+        </LocalMenuLink>
+      </li>
+      <li>
+        <LocalMenuLink to={datasetUsagePath(thematic, dataset)} end>
+          Usage
+        </LocalMenuLink>
+      </li>
+    </LocalMenu>
+  );
+}
+
+DatasetsLocalMenu.propTypes = {
   thematic: T.object,
   dataset: T.object
 };
--- app/scripts/components/discoveries/single/index.js
+++ app/scripts/components/discoveries/single/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-import { LayoutProps } from '../../common/layout-root';
-import { PageMainContent } from '../../../styles/page';
-import PageHero from '../../common/page-hero';
-import { FoldProse } from '../../common/fold';
-import { resourceNotFound } from '../../uhoh';
+import { LayoutProps } from '$components/common/layout-root';
+import PageHero from '$components/common/page-hero';
+import { FoldProse } from '$components/common/fold';
+import { resourceNotFound } from '$components/uhoh';
+import PageLocalNav from '$components/common/page-local-nav';
+import { PageMainContent } from '$styles/page';
 
 import {
   useMdxPageLoader,
   useThematicArea,
   useThematicAreaDiscovery
-} from '../../../utils/thematics';
+} from '$utils/thematics';
+import { thematicDiscoveriesPath } from '$utils/routes';
 
 function DiscoveriesSingle() {
   const thematic = useThematicArea();
@@ -20,22 +22,31 @@ function DiscoveriesSingle() {
   if (!thematic || !discovery) return resourceNotFound();
 
   return (
-    <PageMainContent>
+    <>
       <LayoutProps title={discovery.data.name} />
-      <PageHero
-        title={discovery.data.name}
-        description={discovery.data.description}
-        publishedDate={discovery.data.pubDate}
-        coverSrc='https://picsum.photos/id/1002/2048/1024'
-        coverAlt='Generic placeholder by lorem picsum'
-        attributionAuthor='Lorem Picsum'
-        attributionUrl='https://picsum.photos/'
+      <PageLocalNav
+        parentName='Discovery'
+        parentLabel='Discoveries'
+        parentTo={thematicDiscoveriesPath(thematic)}
+        items={thematic.data.discoveries}
+        currentId={discovery.data.id}
       />
-      <FoldProse>
-        {pageMdx.status === 'loading' && <p>Loading page content</p>}
-        {pageMdx.status === 'success' && <pageMdx.MdxContent />}
-      </FoldProse>
-    </PageMainContent>
+      <PageMainContent>
+        <PageHero
+          title={discovery.data.name}
+          description={discovery.data.description}
+          publishedDate={discovery.data.pubDate}
+          coverSrc='https://picsum.photos/id/1002/2048/1024'
+          coverAlt='Generic placeholder by lorem picsum'
+          attributionAuthor='Lorem Picsum'
+          attributionUrl='https://picsum.photos/'
+        />
+        <FoldProse>
+          {pageMdx.status === 'loading' && <p>Loading page content</p>}
+          {pageMdx.status === 'success' && <pageMdx.MdxContent />}
+        </FoldProse>
+      </PageMainContent>
+    </>
   );
 }
 
