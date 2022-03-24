8c3b5f2ac1e1ee77313affc677611e7d77f00057
Ricardo Mestre <ricardo@duplos.org>
2022-03-24 17:28:17 +0000

    Allow about page to use mdx block functionality

--- app/scripts/components/about/index.js
+++ app/scripts/components/about/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
 
 import { LayoutProps } from '../common/layout-root';
-import { FoldProse } from '../common/fold';
 
 import { PageLead, PageMainContent } from '../../styles/page';
 import { resourceNotFound } from '../uhoh';
 import PageHero from '../common/page-hero';
+import { Caption } from '$components/common/images/';
 
 import { useMdxPageLoader, useThematicArea } from '../../utils/thematics';
+
+import Block from '$components/common/blocks';
+import ContentBlockFigure from '$components/common/blocks/figure';
+import Image from '$components/common/images/';
+import { ContentBlockProse } from '$styles/content-block';
+import Chart from '$components/discoveries/chart/';
 
 function About() {
   const thematic = useThematicArea();
@@ -22,10 +29,21 @@ function About() {
         title={thematic.data.about?.title || 'n/a'}
         detailsContent={<PageLead>{thematic.data.about?.description}</PageLead>}
       />
-      <FoldProse>
-        {pageMdx.status === 'loading' && <p>Loading page content</p>}
-        {pageMdx.status === 'success' && <pageMdx.MdxContent />}
-      </FoldProse>
+      {pageMdx.status === 'loading' && <p>Loading page content</p>}
+      {pageMdx.status === 'success' && (
+        <MDXProvider
+          components={{
+            Block,
+            Prose: ContentBlockProse,
+            Figure: ContentBlockFigure,
+            Caption,
+            Image,
+            Chart
+          }}
+        >
+          <pageMdx.MdxContent />
+        </MDXProvider>
+      )}
     </PageMainContent>
   );
 }
