6040091333b62546f99f25f9f10586262bd69e04
Hanbyul Jo <hanbyul@nyu.edu>
2022-03-30 10:38:28 -0400

    Fix path for blocks related components

--- app/scripts/components/about/index.js
+++ app/scripts/components/about/index.js
@@ -3,18 +3,17 @@ import { MDXProvider } from '@mdx-js/react';
 
 import { LayoutProps } from '../common/layout-root';
 
-import { PageLead, PageMainContent } from '../../styles/page';
+import { PageMainContent } from '../../styles/page';
 import { resourceNotFound } from '../uhoh';
 import PageHero from '../common/page-hero';
-import { Caption } from '$components/common/images/';
 
 import { useMdxPageLoader, useThematicArea } from '../../utils/thematics';
 
 import Block from '$components/common/blocks';
 import ContentBlockFigure from '$components/common/blocks/figure';
-import Image from '$components/common/images/';
+import Image, { Caption } from '$components/common/blocks/images/';
 import { ContentBlockProse } from '$styles/content-block';
-import Chart from '$components/discoveries/chart/';
+import Chart from '$components/common/blocks/chart/';
 
 function About() {
   const thematic = useThematicArea();
