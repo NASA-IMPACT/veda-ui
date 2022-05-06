ea56c49e37df37ff7853f6edd188a5edc60a992f
Daniel da Silva <danielfdsilva@gmail.com>
2022-05-06 13:07:42 +0100

    Normalize statuses

--- app/scripts/utils/status.ts
+++ app/scripts/utils/status.ts
@@ -1,6 +1,6 @@
-export type ActionStatus = 'idle' | 'loading' | 'succeeded' | 'failed';
+export type ActionStatus = 'idle' | 'loading' | 'success' | 'error';
 
 export const S_IDLE = 'idle';
 export const S_LOADING = 'loading';
-export const S_SUCCEEDED = 'succeeded';
-export const S_FAILED = 'failed';
+export const S_SUCCEEDED = 'success';
+export const S_FAILED = 'error';
