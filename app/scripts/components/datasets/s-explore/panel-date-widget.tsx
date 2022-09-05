aeca8fa12d47c9a119b74e218ac7636b88869df3
Daniel da Silva <danielfdsilva@gmail.com>
2022-09-05 16:31:56 +0100

    Add timeseries slider to explore page

--- app/scripts/components/datasets/s-explore/panel-date-widget.tsx
+++ app/scripts/components/datasets/s-explore/panel-date-widget.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
   CollecticonCalendar,
   CollecticonChevronLeftSmall,
@@ -24,6 +24,8 @@ import {
 } from '@devseed-ui/toolbar';
 import { format } from 'date-fns';
 import { mod } from '$utils/utils';
+import TimeseriesControl from './timeseries';
+import { prepareDates } from './timeseries/utils';
 
 function getDatePickerView(timeDensity?: TimeDensity) {
   const view = {
@@ -34,7 +36,7 @@ function getDatePickerView(timeDensity?: TimeDensity) {
     // If the data's time density is monthly only allow the user to select a
     // month by setting the picker to a early view.
     year: 'decade'
-  }[timeDensity || 'day'];
+  }[timeDensity || 'month'];
 
   return view as Exclude<DropdownDatePickerProps['view'], undefined>;
 }
@@ -76,6 +78,14 @@ export function PanelDateWidget(props: PanelDateWidgetProps) {
   const currIndex =
     availableDates?.findIndex((d) => d.getTime() === value.start?.getTime()) ??
     -1;
+
+  const timeSliderDates = useMemo(
+    () =>
+      availableDates && timeDensity
+        ? prepareDates(availableDates, timeDensity)
+        : null,
+    [availableDates, timeDensity]
+  );
 
   return (
     <PanelWidget>
@@ -144,6 +154,14 @@ export function PanelDateWidget(props: PanelDateWidgetProps) {
               />
             </Toolbar>
           </WidgetItemHGroup>
+          {timeSliderDates && timeDensity && (
+            <TimeseriesControl
+              data={timeSliderDates}
+              value={value.start || undefined}
+              timeDensity={timeDensity}
+              onChange={({ date }) => onConfirm({ start: date, end: date })}
+            />
+          )}
           {children}
         </WidgetItemHeader>
       </PanelWidgetBody>
