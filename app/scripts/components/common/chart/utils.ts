ad7f51ae06408523a07fe098b241e01d33ced08b
Hanbyul Jo <hanbyul@nyu.edu>
2022-09-06 15:15:19 -0400

    Add comment for format data logic

--- app/scripts/components/common/chart/utils.ts
+++ app/scripts/components/common/chart/utils.ts
@@ -1,8 +1,6 @@
 import { timeFormat, timeParse } from 'd3-time-format';
 import * as d3ScaleChromatic from 'd3-scale-chromatic';
 
-const groupBy = require('lodash/groupby');
-
 export const dateFormatter = (date: Date, dateFormat: string) => {
   const format = timeFormat(dateFormat);
   return format(date);
@@ -17,11 +15,11 @@ export const convertToTime = ({ timeString, dateFormat}: {timeString: string, da
 
 /**
  * Returns 
- * uniqueKeys: an array of unique keys to represent each line of the chart (and accompanying components such as legend, tooltip)
- * fData: data to feed chart. one unit looks like below
- * { xKeyValue: Date, otherPropertiesDrivenFromUniqueKeys }
- * if uniqueKeys are =['no2', 'so2'], xKey is Year, the object will look like
- * { Year: Date, no2: value, so2: value}
+ * {
+ *    uniqueKeys: an array of unique keys to represent each line of the chart (and accompanying components such as legend, tooltip)
+ *    fData: data to feed chart. one unit looks like: { xKeyValue: Date, otherPropertiesDrivenFromUniqueKeys }
+ * }
+ * ex. If uniqueKeys are =['no2', 'so2'], xKey is Year, the unit will look like { Year: Date, no2: value, so2: value}
  * 
  * @param {object[]} data 
  * @param {string} idKey The key or getter of a group of data which should be unique in a Chart
@@ -33,27 +31,31 @@ export const convertToTime = ({ timeString, dateFormat}: {timeString: string, da
 export function getFData({ data, idKey, xKey, yKey, dateFormat }: { data: any[], idKey: string, xKey: string, yKey: string, dateFormat: string }) {
   const uniqueKeys = [...Array.from(new Set(data.map((d) => d[idKey])))].sort();
 
-  const fData: object[] = [];
+  // Format csv/json data into chart suitable data
+  // ex. When XKey is Year, uniqueKeys are ['no2', 'so2']
+  // Format [{Year: 2005, no2: 0 }, {Year: 2005, so2: 10}] to [{Year: 2005, no2: 0, so2: 10}]
+  // Using an object(keyObject) with xKeyValue, so we can process data without iteration. 
+  // keyObject is shaped like: {2005: [{no2: 0, so2:10}]} 
+  // Once the array length equals to uniqueKeys length, (once all the needed values were saved)
+  // the unit gets properly formatted like below and is pushed to final data to return
+  // [{Year: 2005, no2: 0, so2: 10},...]
 
-  // Format ex. [{Year: 2005, no2: 0 }, {Year: 2005, so2: 10}] to [{Year: 2005, no2: 0, so2: 10}]
-  // Using an object with xKeyValue as Keys as a midpoint
-  // ex. {2005: [{no2: 0, so2:10}]} 
-  // once the array length equals to reaches to uniqueKeys length, the unit gest pushed to final data to return
-  data.reduce((acc, curr) => {
+  const { fData } = data.reduce(({ keyObject, fData}, curr) => {
     // Use acc object so we don't have to iterate (ex.Array.find) the array to find an element
-    if (!acc[curr[xKey]]) {
-      acc[curr[xKey]] = {
+    if (!keyObject[curr[xKey]]) {
+      keyObject[curr[xKey]] = {
         [curr[idKey]]: parseFloat(curr[yKey])
       };
     } else {
-      acc[curr[xKey]] = {
-        ...acc[curr[xKey]],
+      keyObject[curr[xKey]] = {
+        ...keyObject[curr[xKey]],
         [curr[idKey]]: parseFloat(curr[yKey])
       };
-      // once the array length equals to reaches to uniqueKeys length, the unit gest pushed to final data to return
-      if (Object.keys(acc[curr[xKey]]).length === uniqueKeys.length) {
+    // Once the array length equals to uniqueKeys length, (once all the needed values were saved)
+    // the unit gest properly formatted and pushed to final data to return
+      if (Object.keys(keyObject[curr[xKey]]).length === uniqueKeys.length) {
         fData.push({
-          ...acc[curr[xKey]],
+          ...keyObject[curr[xKey]],
           [xKey]: convertToTime({
             timeString: curr[xKey],
             dateFormat
@@ -62,8 +64,12 @@ export function getFData({ data, idKey, xKey, yKey, dateFormat }: { data: any[],
       }
     }
 
-    return acc;
-  }, {});
+    return {
+      keyObject,
+      fData
+    };
+  }, { keyObject: {}, fData:[]});
+
 
   return {
     uniqueKeys,
@@ -71,7 +77,7 @@ export function getFData({ data, idKey, xKey, yKey, dateFormat }: { data: any[],
   };
 }
 
-function getInterpoateFunction(colorScheme: string) {
+function getInterpolateFunction(colorScheme: string) {
   const fnName = `interpolate${
     colorScheme[0].toUpperCase() + colorScheme.slice(1)
   }`;
@@ -79,7 +85,7 @@ function getInterpoateFunction(colorScheme: string) {
 }
 
 export const getColors = function ({ steps, colorScheme } : {steps: number, colorScheme: string}) {
-  const colorFn = getInterpoateFunction(colorScheme);
+  const colorFn = getInterpolateFunction(colorScheme);
   return new Array(steps).fill(0).map((e, idx) => {
     return colorFn(idx / steps);
   });
