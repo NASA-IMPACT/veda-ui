b02de80bcbe87666a5f909e760da1459d726d955
Daniel da Silva <danielfdsilva@gmail.com>
2023-01-05 16:24:55 +0000

    Add browser frames to value proposition

--- app/scripts/components/root-home/value-propostion.tsx
+++ app/scripts/components/root-home/value-propostion.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import imgVP01 from '../../../graphics/layout/vp-01-illu.png';
+import imgVP02 from '../../../graphics/layout/vp-02-illu.png';
+import imgVP03 from '../../../graphics/layout/vp-03-illu.png';
+
 import {
   FoldHeader,
   FoldTitle,
@@ -14,10 +18,7 @@ import {
 } from '$components/common/blocks';
 import ContentBlockFigure from '$components/common/blocks/figure';
 import { ContentBlockProse } from '$styles/content-block';
-
-import imgVP01 from '../../../graphics/layout/vp-01-illu.png';
-import imgVP02 from '../../../graphics/layout/vp-02-illu.png';
-import imgVP03 from '../../../graphics/layout/vp-03-illu.png';
+import BrowserFrame from '$styles/browser-frame';
 
 const ContentBlockProseAlt = styled(ContentBlockProse)`
   h1,
@@ -59,13 +60,17 @@ function ValueProposition() {
             </p>
           </ContentBlockProseAlt>
           <ContentBlockFigure>
-            <img src={imgVP01} alt='Screenshot of VEDA in action.' />
+            <BrowserFrame>
+              <img src={imgVP01} alt='Screenshot of VEDA in action.' />
+            </BrowserFrame>
           </ContentBlockFigure>
         </ContentBlockPFBeta>
 
         <ContentBlockPFAlpha>
           <ContentBlockFigure>
-            <img src={imgVP02} alt='Screenshot of VEDA in action.' />
+            <BrowserFrame>
+              <img src={imgVP02} alt='Screenshot of VEDA in action.' />
+            </BrowserFrame>
           </ContentBlockFigure>
           <ContentBlockProseAlt>
             <h3>Cloud-enabled scientific analysis</h3>
@@ -85,7 +90,9 @@ function ValueProposition() {
             </p>
           </ContentBlockProseAlt>
           <ContentBlockFigure>
-            <img src={imgVP03} alt='Screenshot of VEDA in action.' />
+            <BrowserFrame>
+              <img src={imgVP03} alt='Screenshot of VEDA in action.' />
+            </BrowserFrame>
           </ContentBlockFigure>
         </ContentBlockPFBeta>
       </FoldBody>
