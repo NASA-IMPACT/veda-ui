d0812b1cdfd8c3d8f5a2cf4660c7d90f162ff7d8
Erik Escoffier <me@erik.nz>
2023-03-01 12:02:00 +0100

    Fix load style at map mount

--- app/scripts/components/common/mapbox/map-options/use-basemap.ts
+++ app/scripts/components/common/mapbox/map-options/use-basemap.ts
@@ -51,7 +51,7 @@ export function useBasemap(mapInstance: mapboxgl.Map | null) {
     // Any BaseLayerComponent (ie raster-timeseries or others) must listen to styledata/styledataloading events
     // on the mgl instance to re-add layers
     mapInstance.setStyle(styleUrl);
-  }, [styleUrl]);
+  }, [mapInstance, styleUrl]);
 
   const [labelsOption, setLabelsOption] = useState(true);
   const [boundariesOption, setBoundariesOption] = useState(true);
@@ -67,7 +67,7 @@ export function useBasemap(mapInstance: mapboxgl.Map | null) {
   );
 
   // Apply labels and boundaries options, by setting visibility on related layers
-  // For simplicity's sake, the Mapbox layer group (as set in Mapbox Studio) is used 
+  // For simplicity's sake, the Mapbox layer group (as set in Mapbox Studio) is used
   // to determine wehether a layer is a labels layer or boundaries or none of those.
   useEffect(() => {
     if (!mapInstance || !styleLoaded) return;
@@ -77,14 +77,17 @@ export function useBasemap(mapInstance: mapboxgl.Map | null) {
         const layerGroup = (layer as Layer).metadata?.['mapbox:group'];
 
         if (layerGroup) {
+          const isLabelsLayer = GROUPS_BY_OPTION.labels.includes(layerGroup);
+          const isBoundariesLayer =
+            GROUPS_BY_OPTION.boundaries.includes(layerGroup);
+
           const visibility =
-            (GROUPS_BY_OPTION.labels.includes(layerGroup) && labelsOption) ||
-            (GROUPS_BY_OPTION.boundaries.includes(layerGroup) &&
-              boundariesOption)
+            (isLabelsLayer && labelsOption) ||
+            (isBoundariesLayer && boundariesOption)
               ? 'visible'
               : 'none';
 
-          if ((layer as Layer).layout?.visibility !== visibility) {
+          if ((isLabelsLayer || isBoundariesLayer) && (layer as Layer).layout?.visibility !== visibility) {
             mapInstance.setLayoutProperty(layer.id, 'visibility', visibility);
           }
         }
