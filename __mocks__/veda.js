3d0abf2eeb2649f10ec0ed1720ffa18c384cd709
Daniel da Silva <danielfdsilva@gmail.com>
2023-05-18 14:30:15 +0100

    Add veda module mock for jest

---
+++ __mocks__/veda.js
@@ -0,0 +1,7 @@
+/*
+  Mock for the veda module for tests.
+  This is needed because since veda is a module resolved by parcel it does not
+  exist in the test environment.
+*/
+
+export const theme = {};
