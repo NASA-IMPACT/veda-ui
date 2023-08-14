263817560850f8d8891f163b3bdef8614cd7d45d
Slesa Adhikari <slesaad@gmail.com>
2023-08-14 11:38:49 -0500

    Move `DATA_BROWSER_PATH` url to an env var

--- app/scripts/utils/routes.ts
+++ app/scripts/utils/routes.ts
@@ -5,7 +5,6 @@ export const STORIES_PATH = '/stories';
 export const DATASETS_PATH = '/data-catalog';
 export const ANALYSIS_PATH = '/analysis';
 export const ANALYSIS_RESULTS_PATH = '/analysis/results';
-export const DATA_BROWSER_PATH = 'https://d36s2ep3ahcq5b.cloudfront.net/browseui/index.html#';
 
 export const getStoryPath = (d: StoryData | string) =>
   `${STORIES_PATH}/${typeof d === 'string' ? d : d.id}`;
@@ -17,5 +16,5 @@ export const getDatasetExplorePath = (d: DatasetData | string) =>
   `${DATASETS_PATH}/${typeof d === 'string' ? d : d.id}/explore`;
 
 export const getDatasetBrowserPath = (d: DatasetData | string) => {
-  return `${DATA_BROWSER_PATH}${typeof d === 'string' ? d : d.dataBucketPrefix}/`;
+  return `${process.env.DATA_BROWSER_PATH}${typeof d === 'string' ? d : d.dataBucketPrefix}/`;
 }
