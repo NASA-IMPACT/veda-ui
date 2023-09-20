e9eac6f4bfeb2cddc9d5c5a5a77d6a23ee358b27
Hanbyul Jo <hanbyul@nyu.edu>
2023-09-20 14:58:40 -0400

    Use collection endpoint instead of search for analysis page

--- app/scripts/components/analysis/define/use-stac-search.ts
+++ app/scripts/components/analysis/define/use-stac-search.ts
@@ -1,10 +1,13 @@
 import { DatasetLayer } from 'veda';
 import { FeatureCollection, Polygon } from 'geojson';
 import { useEffect, useState } from 'react';
-import { uniq } from 'lodash';
 import axios from 'axios';
-import { getFilterPayload } from '../utils';
+import booleanIntersects from '@turf/boolean-intersects';
+import bboxPolygon from '@turf/bbox-polygon';
+import { areIntervalsOverlapping } from 'date-fns';
+
 import { allAvailableDatasetsLayers } from '.';
+import { utcString2userTzDate } from '$utils/date';
 import {
   ActionStatus,
   S_FAILED,
@@ -22,7 +25,6 @@ interface UseStacSearchProps {
 export function useStacSearch({ start, end, aoi }: UseStacSearchProps) {
   const readyToLoadDatasets = !!(start && end && aoi);
 
-
   const [selectableDatasetLayers, setSelectableDatasetLayers] = useState<
     DatasetLayer[]
   >([]);
@@ -37,43 +39,44 @@ export function useStacSearch({ start, end, aoi }: UseStacSearchProps) {
     const load = async () => {
       setStacSearchStatus(S_LOADING);
       try {
-        const url = `${process.env.API_STAC_ENDPOINT}/search`;
-
-        const allAvailableDatasetsLayersSTACIds = allAvailableDatasetsLayers.map(
-          (layer) => layer.stacCol
-        );
-        const payload = {
-          'filter-lang': 'cql2-json',
-          filter: getFilterPayload(
-            start,
-            end,
-            aoi,
-            allAvailableDatasetsLayersSTACIds
-          ),
-          limit: 100,
-          fields: {
-            exclude: [
-              'links',
-              'assets',
-              'bbox',
-              'geometry',
-              'properties',
-              'stac_extensions',
-              'stac_version',
-              'type'
-            ]
-          }
-        };
-        const response = await axios.post(url, payload, {
+        const collectionUrl = `${process.env.API_STAC_ENDPOINT}/collections`;
+        const collectionResponse = await axios.get(collectionUrl, {
           signal: controller.signal
         });
+
+        const matchingCollectionIds = collectionResponse.data.collections
+          .filter(
+            (col) => col.extent.spatial.bbox && col.extent.temporal.interval
+          )
+          .map((col) => {
+            return {
+              id: col.id,
+              bbox: col.extent.spatial.bbox[0], // Check
+              start: utcString2userTzDate(col.extent.temporal.interval[0][0]),
+              end: utcString2userTzDate(col.extent.temporal.interval[0][1])
+            };
+          })
+          .filter((col) => {
+            return (
+              aoi.features.some((feature) =>
+                booleanIntersects(feature, bboxPolygon(col.bbox))
+              ) &&
+              areIntervalsOverlapping(
+                { start: new Date(start), end: new Date(end) },
+                {
+                  start: new Date(col.start),
+                  end: new Date(col.end)
+                }
+              )
+            );
+          })
+          .map((c) => c.id);
+
         setStacSearchStatus(S_SUCCEEDED);
-        const itemsParentCollections: string[] = uniq(
-          response.data.features.map((feature) => feature.collection)
-        );
+
         setSelectableDatasetLayers(
           allAvailableDatasetsLayers.filter((l) =>
-            itemsParentCollections.includes(l.stacCol)
+            matchingCollectionIds.includes(l.stacCol)
           )
         );
       } catch (error) {
