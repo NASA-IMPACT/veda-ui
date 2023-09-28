143f695d429c98b26cf733190aeb94e0c438bfea
Hanbyul Jo <hanbyul@nyu.edu>
2023-09-28 11:59:28 -0400

    Reberts navlink back to use react router for testing

--- app/scripts/components/common/page-header.tsx
+++ app/scripts/components/common/page-header.tsx
@@ -399,8 +399,7 @@ function PageHeader() {
                 <GlobalMenu>
                   <li>
                     <GlobalMenuLink
-                      as='a'
-                      href={DATASETS_PATH}
+                      to={DATASETS_PATH}
                       onClick={closeNavOnClick}
                     >
                       Data Catalog
@@ -408,19 +407,14 @@ function PageHeader() {
                   </li>
                   <li>
                     <GlobalMenuLink
-                      as='a'
-                      href={ANALYSIS_PATH}
+                      to={ANALYSIS_PATH}
                       onClick={closeNavOnClick}
                     >
                       Data Analysis
                     </GlobalMenuLink>
                   </li>
                   <li>
-                    <GlobalMenuLink
-                      as='a'
-                      href={STORIES_PATH}
-                      onClick={closeNavOnClick}
-                    >
+                    <GlobalMenuLink to={STORIES_PATH} onClick={closeNavOnClick}>
                       {getString('stories').other}
                     </GlobalMenuLink>
                   </li>
