4f6d556411f87038364f93ffe0e499da4dcf0823
Daniel da Silva <danielfdsilva@gmail.com>
2023-10-11 18:19:58 +0100

    Add preliminary map message

---
+++ app/scripts/components/exploration/components/map/analysis-message.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect } from 'react';
+import { useAtomValue } from 'jotai';
+import styled from 'styled-components';
+import { Button } from '@devseed-ui/button';
+import {
+  CollecticonChartLine,
+  CollecticonXmarkSmall
+} from '@devseed-ui/collecticons';
+
+import { selectedIntervalAtom } from '../../atoms/atoms';
+
+import useAois from '$components/common/map/controls/hooks/use-aois';
+import { calcFeatCollArea } from '$components/common/aoi/utils';
+import { formatDateRange } from '$utils/date';
+import { useAnalysisController } from '$components/exploration/hooks/use-analysis-data-request';
+
+const AnalysisMessageWrapper = styled.div`
+  position: absolute;
+  background-color: #fff;
+  top: 2rem;
+  left: 5rem;
+  padding: 0.25rem 0.5rem;
+`;
+
+export function AnalysisMessage() {
+  const { isObsolete, setObsolete, runAnalysis, cancelAnalysis, isAnalyzing } =
+    useAnalysisController();
+
+  const { features } = useAois();
+  const selectedInterval = useAtomValue(selectedIntervalAtom);
+  const dateLabel =
+    selectedInterval &&
+    formatDateRange(selectedInterval.start, selectedInterval.end);
+
+  const selectedFeatures = features.filter((f) => f.selected);
+  const selectedFeatureIds = selectedFeatures.map((f) => f.id).join(',');
+
+  useEffect(() => {
+    // Set the analysis as obsolete when the selected features change.
+    setObsolete();
+  }, [setObsolete, selectedFeatureIds]);
+
+  if (!selectedFeatures.length) return null;
+
+  const area = calcFeatCollArea({
+    type: 'FeatureCollection',
+    features: selectedFeatures
+  });
+
+  return (
+    <AnalysisMessageWrapper>
+      An area of {area} km<sup>2</sup> is selected
+      {dateLabel && ` from ${dateLabel}`}.{' '}
+      {isAnalyzing ? (
+        <Button
+          variation='base-text'
+          size='small'
+          onClick={() => {
+            cancelAnalysis();
+          }}
+        >
+          <CollecticonXmarkSmall /> Cancel
+        </Button>
+      ) : (
+        <Button
+          variation='base-text'
+          size='small'
+          onClick={() => {
+            runAnalysis();
+          }}
+        >
+          <CollecticonChartLine /> Analyze
+        </Button>
+      )}
+      {isAnalyzing && isObsolete && (
+        <>
+          The current analysis is obsolete.{' '}
+          <Button
+            variation='base-text'
+            size='small'
+            onClick={() => {
+              runAnalysis();
+            }}
+          >
+            <CollecticonChartLine /> Update
+          </Button>
+        </>
+      )}
+    </AnalysisMessageWrapper>
+  );
+}
--- app/scripts/components/exploration/components/map/index.tsx
+++ app/scripts/components/exploration/components/map/index.tsx
@@ -9,6 +9,7 @@ import {
   TimelineDatasetSuccess
 } from '../../types.d.ts';
 import { Layer } from './layer';
+import { AnalysisMessage } from './analysis-message';
 
 import Map, { Compare } from '$components/common/map';
 import { Basemap } from '$components/common/map/style-generators/basemap';
@@ -49,10 +50,11 @@ export function ExplorationMap(props: { comparing: boolean }) {
     .reverse();
 
   const { onUpdate, onDelete, onSelectionChange } = useAois();
-  // console.log(features);
 
   return (
     <Map id='exploration' projection={projection}>
+      <AnalysisMessage />
+
       {/* Map layers */}
       <Basemap
         basemapStyleId={mapBasemapId}
