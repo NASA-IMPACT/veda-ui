84e13f2064e58017efd0b8d8a45c1561f7d126d2
Daniel da Silva <danielfdsilva@gmail.com>
2023-10-12 09:48:09 +0100

    Include scrollbar width in media calculation
    Fix #697

--- app/scripts/utils/use-scrollbar-width-css.ts
+++ app/scripts/utils/use-scrollbar-width-css.ts
@@ -1,16 +1,27 @@
 import { useEffect } from 'react';
 
+let scrollbarWidthCache;
+export function getScrollbarWidth() {
+  if (scrollbarWidthCache !== undefined) {
+    return scrollbarWidthCache;
+  }
+
+  const el = document.createElement('div');
+  el.style.cssText = 'overflow:scroll; visibility:hidden; position:absolute;';
+  document.body.appendChild(el);
+  const width = el.offsetWidth - el.clientWidth;
+  el.remove();
+
+  scrollbarWidthCache = width;
+  return width;
+}
+
 export function useScrollbarWidthAsCssVar(varName = '--scrollbar-width') {
   useEffect(() => {
-    const el = document.createElement('div');
-    el.style.cssText = 'overflow:scroll; visibility:hidden; position:absolute;';
-    document.body.appendChild(el);
-    const width = el.offsetWidth - el.clientWidth;
-    el.remove();
-
+    const width = getScrollbarWidth();
     document.documentElement.style.setProperty(varName, width + 'px');
 
-    () => {
+    return () => {
       document.documentElement.style.removeProperty(varName);
     };
   }, [varName]);
