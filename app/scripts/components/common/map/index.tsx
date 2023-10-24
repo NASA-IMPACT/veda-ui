9738d47c28c724a4b27d9ab5dc53ccaf1db033ea
Daniel da Silva <danielfdsilva@gmail.com>
2023-10-24 15:37:48 +0100

    Use a wrapper for map controls
    Co-authored-by: Hanbyul Jo <hanbyul@nyu.edu>

--- app/scripts/components/common/map/index.tsx
+++ app/scripts/components/common/map/index.tsx
@@ -2,9 +2,19 @@ import React, { ReactNode } from 'react';
 import { MapProvider } from 'react-map-gl';
 import Maps, { MapsContextWrapperProps } from './maps';
 
+export const COMPARE_CONTAINER_NAME = 'CompareContainer';
 export function Compare({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
+
+Compare.displayName = COMPARE_CONTAINER_NAME;
+
+export const CONTROLS_CONTAINER_NAME = 'MapControlsContainer';
+export function MapControls({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
+
+MapControls.displayName = CONTROLS_CONTAINER_NAME;
 
 export default function MapProviderWrapper(props: MapsContextWrapperProps) {
   return (
