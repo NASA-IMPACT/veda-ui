fc8a0a72bdb11a49d51903bf1029d8fc9cdec0cd
Daniel da Silva <danielfdsilva@gmail.com>
2023-11-15 15:32:06 +0000

    Remove analysis from menu

--- app/scripts/utils/routes.ts
+++ app/scripts/utils/routes.ts
@@ -3,8 +3,6 @@ import { DatasetData, StoryData } from 'veda';
 export const ABOUT_PATH = '/about';
 export const STORIES_PATH = '/stories';
 export const DATASETS_PATH = '/data-catalog';
-export const ANALYSIS_PATH = '/analysis';
-export const ANALYSIS_RESULTS_PATH = '/analysis/results';
 export const EXPLORATION_PATH = '/exploration';
 
 export const getStoryPath = (d: StoryData | string) =>
