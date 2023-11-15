bc5f4af1af23b5f72912c79acdc4dad0d2ad5aee
Daniel da Silva <danielfdsilva@gmail.com>
2023-11-15 18:37:44 +0000

    Add analysis paths back
    Needed while the code is not deleted to prevent ts errors

--- app/scripts/utils/routes.ts
+++ app/scripts/utils/routes.ts
@@ -4,6 +4,8 @@ export const ABOUT_PATH = '/about';
 export const STORIES_PATH = '/stories';
 export const DATASETS_PATH = '/data-catalog';
 export const EXPLORATION_PATH = '/exploration';
+export const ANALYSIS_PATH = '/analysis';
+export const ANALYSIS_RESULTS_PATH = '/analysis/results';
 
 export const getStoryPath = (d: StoryData | string) =>
   `${STORIES_PATH}/${typeof d === 'string' ? d : d.id}`;
