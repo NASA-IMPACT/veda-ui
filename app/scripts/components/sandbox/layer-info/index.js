26a904a16c1063a1c1839cd1e866b8c71bcd7a64
Hanbyul Jo <hanbyul@nyu.edu>
2024-02-13 16:48:47 -0500

    Add table data usage

--- app/scripts/components/sandbox/layer-info/index.js
+++ app/scripts/components/sandbox/layer-info/index.js
@@ -7,11 +7,22 @@ import { ContentBlockProse } from '$styles/content-block';
 export default function SandboxMDXPage() {
   const data = datasets['no2'].data.layers[0];
   const layerInfoMdxContent = data.info?.description;
+  const layerInfoTable = data.info.table;
+
   return layerInfoMdxContent ? (
     <Block>
       <ContentBlockProse>
         <h2>{data.name} Layer Info</h2>
         <div dangerouslySetInnerHTML={{ __html: layerInfoMdxContent }} />
+        <ul>
+          {Object.keys(layerInfoTable).map((key) => {
+            return (
+              <li key={key}>
+                <b>{key} : </b> <span>{layerInfoTable[key]}</span>
+              </li>
+            );
+          })}
+        </ul>
       </ContentBlockProse>
     </Block>
   ) : (
