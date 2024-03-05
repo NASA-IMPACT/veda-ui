7f8e64b1cb3858e211724be2ee59d186ae31b07a
Raj Dangol <rd0077@uah.edu>
2024-03-05 12:26:07 -0600

    fixed lint issue

--- app/scripts/components/common/mdx-content.js
+++ app/scripts/components/common/mdx-content.js
@@ -8,7 +8,7 @@ import { ContentLoading } from '$components/common/loading-skeleton';
 import Block from '$components/common/blocks';
 import ContentBlockFigure from '$components/common/blocks/figure';
 import { ContentBlockProse } from '$styles/content-block';
-import Image, { 
+import Image, {
   Caption,
   FullMapLinkButton
 } from '$components/common/blocks/images';
