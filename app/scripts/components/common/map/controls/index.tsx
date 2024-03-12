2b5cee0a8ced4ffd34fd0f3b76a986502637daac
Sandra Hoang <sandrahoang686@gmail.com>
2024-03-12 14:30:55 -0400

    Fix when no compare date and zoom control location

--- app/scripts/components/common/map/controls/index.tsx
+++ app/scripts/components/common/map/controls/index.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import {
   NavigationControl as MapboxGLNavigationControl,
   ScaleControl as MapboxGLScaleControl,
-  AttributionControl as MapboxAttributionControl
+  AttributionControl as MapboxAttributionControl,
+  ControlPosition
 } from 'react-map-gl';
 
 export function AttributionControl({message}: {message: string}) {
   return <MapboxAttributionControl customAttribution={message} compact={true} />;
 }
 
-export function NavigationControl() {
-  return <MapboxGLNavigationControl position='top-right' showCompass={false} />;
+export function NavigationControl({position='top-right'}: {position?: ControlPosition}) {
+  return <MapboxGLNavigationControl position={position} showCompass={false} />;
 }
 
 export function ScaleControl() {
