a6946508ae4df2b3c9756238679da19907a8adb2
Oliver Roick <oliver@roick.email>
2024-05-30 11:04:08 +1000

    Fix named imports

--- app/scripts/index.ts
+++ app/scripts/index.ts
@@ -1,45 +1,31 @@
 import DataCatalog from './components/common/catalog';
 
 import Block from './components/common/blocks';
-import ContentBlockFigure from './components/common/blocks/figure';
-import { ContentBlockProse } from './styles/content-block';
-import Image, { Caption } from './components/common/blocks/images';
+import Figure from './components/common/blocks/figure';
+import { ContentBlockProse as Prose } from './styles/content-block';
+import MDXImage, { Caption } from './components/common/blocks/images';
 import { Chapter } from './components/common/blocks/scrollytelling/chapter';
 
-import BlockChart from './components/common/chart/block';
+import Chart from './components/common/chart/block';
 
 import Table from './components/common/table';
 import CompareImage from './components/common/blocks/images/compare';
 
 
 import Embed from './components/common/blocks/embed';
-import DevseedUiThemeProvider from './theme-provider';
+import TProvider from './theme-provider';
 
 export {
   DataCatalog,
   Block,
-  ContentBlockFigure as Figure,
-  ContentBlockProse as Prose,
+  Figure,
+  Prose,
   CompareImage,
-  Image as MDXImage,
+  MDXImage,
   Caption,
   Chapter,
-  BlockChart as Chart,
+  Chart,
   Table,
   Embed,
-  DevseedUiThemeProvider as TProvider
+  TProvider
 };
-
-export default ({
-  DataCatalog,
-  Block,
-  Figure: ContentBlockFigure,
-  Prose: ContentBlockProse,
-  CompareImage,
-  MDXImage: Image,
-  Caption: Caption,
-  Chapter,
-  Chart: BlockChart,
-  Table,
-  Embed
-});
