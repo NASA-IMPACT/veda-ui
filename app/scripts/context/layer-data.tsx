973ce19b163b18068b1168fbe069026e14adfb9d
Aimee Barciauskas <aimee@developmentseed.org>
2024-06-10 12:45:58 -0700

    Ongoing dataset support

--- app/scripts/context/layer-data.tsx
+++ app/scripts/context/layer-data.tsx
@@ -52,25 +52,14 @@ const fetchLayerById = async (
         domain: featuresApiData.extent.temporal.interval[0]
       }
     };
-  } else if (type === 'cmr') {
-    const domain = data.summaries?.datetime?.[0]
-      ? data.summaries.datetime
-      : data.extent.temporal.interval[0];
-    const domainStart = domain[0];
-    
-    // CMR STAC returns datetimes with `null` as the last value to indicate ongoing data.
-    const lastDatetime = domain[domain.length - 1] ||  new Date().toISOString();
-    
-    return {
-      timeseries: {
-        ...commonTimeseriesParams,
-        domain: [domainStart, lastDatetime]
-      }
-    };
   } else {
     const domain = data.summaries?.datetime?.[0]
       ? data.summaries.datetime
       : data.extent.temporal.interval[0];
+    const domain_length = domain.length;
+    if (domain[domain_length - 1] == null) {
+      domain[domain_length - 1] = new Date().toISOString();
+    }
 
     if (domain.some((d) => !d)) {
       throw new Error('Invalid datetime domain');
