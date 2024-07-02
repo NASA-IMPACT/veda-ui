fcd9f64baf1e252c989fc4a8e079776e91b89904
Hanbyul Jo <hanbyul@nyu.edu>
2024-07-02 13:25:26 -0400

    Use uncss as a plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   syntax: 'postcss-scss',
   parser: 'postcss-safe-parser',
-  plugins: [require('autoprefixer'), require('postcss-import')]
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-import'),
+    require('postcss-uncss')({
+      html: './app/index.html'
+    })
+  ]
 };
