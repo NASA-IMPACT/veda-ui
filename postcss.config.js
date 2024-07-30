5f21b87cf1745fead333d66696f5e79ab30d3105
Gjore Milevski <dzole0311@gmail.com>
2024-07-30 13:54:20 +0200

    Make the path to the node package also dynamic

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,20 @@ const fs = require('fs-extra');
 const CWD = process.cwd();
 
 const vedaPath = path.resolve(CWD, '.veda/ui');
-
 const isVedaInstance = fs.existsSync(vedaPath);
+
+let uswdsPath = path.resolve(CWD, 'node_modules/@trussworks/react-uswds/lib');
+
+if (isVedaInstance)
+  uswdsPath = path.resolve(
+    CWD,
+    '.veda/ui',
+    'node_modules/@trussworks/react-uswds/lib'
+  );
+
 const contentPaths = isVedaInstance
-  ? [
-      '.veda/ui/app/**/*.{js,jsx,ts,tsx}',
-      '@trussworks/react-uswds/lib/index.css'
-    ]
-  : ['./app/**/*.{js,jsx,ts,tsx}', '@trussworks/react-uswds/lib/index.css'];
+  ? ['.veda/ui/app/**/*.{js,jsx,ts,tsx}', `${uswdsPath}/index.css`]
+  : ['./app/**/*.{js,jsx,ts,tsx}', `${uswdsPath}/index.css`];
 
 let plugins = [require('autoprefixer'), require('postcss-import')];
 const purge = require('@fullhuman/postcss-purgecss')({
