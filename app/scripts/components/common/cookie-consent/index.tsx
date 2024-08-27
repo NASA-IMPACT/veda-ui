35f324f8c434e3cf0c984f700bd0e50c4fda6450
snmln <smalone@element84.com>
2024-08-27 09:59:35 -0400

    cleaning up padding

--- app/scripts/components/common/cookie-consent/index.tsx
+++ app/scripts/components/common/cookie-consent/index.tsx
@@ -1,16 +1,31 @@
-import { Alert, Button, ButtonGroup, Link } from "@trussworks/react-uswds";
-import React from "react";
-import './index.scss'
+import { Alert, Button, ButtonGroup, Link } from '@trussworks/react-uswds';
+import React from 'react';
+import './index.scss';
 export const CookieConsent = () => {
-    return (
-        <div className="modal">
-            <Alert
-                type="info"
-                heading='Cookie Consent'
-                headingLevel='h1'
-                noIcon={true}
-            >We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our <Link href='https://www.nasa.gov/privacy/#cookies' >Privacy Policy</Link>
-                <ButtonGroup><Button outline={true} type={"button"} >Decline Cookies</Button> <Button type={"button"} >Accept Cookies</Button></ButtonGroup>
-            </Alert>
-        </div>)
-}
+  return (
+    <div className='modal margin-2'>
+      <Alert
+        type='info'
+        heading='Cookie Consent'
+        headingLevel='h1'
+        noIcon={true}
+      >
+        <div className='padding-bottom-2'>
+          We use cookies to enhance your browsing experience and to help us
+          understand how our website is used. These cookies allow us to collect
+          data on site usage and improve our services based on your
+          interactions. To learn more about it, see our{' '}
+          <Link href='https://www.nasa.gov/privacy/#cookies'>
+            Privacy Policy
+          </Link>
+        </div>
+        <ButtonGroup>
+          <Button outline={true} type={'button'}>
+            Decline Cookies
+          </Button>
+          <Button type={'button'}>Accept Cookies</Button>
+        </ButtonGroup>
+      </Alert>
+    </div>
+  );
+};
