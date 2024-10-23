8d0d1656771eab4421d03bdc3683a6b8f0ba68e4
snmln <smalone@element84.com>
2024-10-23 15:06:40 -0400

    updating test and close consent functionality

--- app/scripts/components/common/cookie-consent/cookieConsent.spec.js
+++ app/scripts/components/common/cookie-consent/cookieConsent.spec.js
@@ -2,6 +2,10 @@ import React from 'react';
 import '@testing-library/jest-dom';
 
 import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // For testing
+
+import { createMemoryHistory } from 'history';
+
 import { COOKIE_CONSENT_KEY, SESSION_KEY } from './utils';
 import { CookieConsent } from './index';
 
@@ -10,12 +14,26 @@ describe('Cookie consent form should render with correct content.', () => {
     title: 'Cookie Consent',
     copy: '<p>We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our <a href="https://www.nasa.gov/privacy/#cookies">Privacy Policy</a></p>We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our [Privacy Policy](https://www.nasa.gov/privacy/#cookies)'
   };
+
   const onFormInteraction = jest.fn();
+
+  const history = createMemoryHistory({ initialEntries: ['/home'] });
+
+  jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useLocation: () => ({
+      pathname: 'localhost:3000/example/path'
+    })
+  }));
+
   beforeEach(() => {
     render(
-      <CookieConsent {...cookieData} onFormInteraction={onFormInteraction} />
+      <MemoryRouter history={history}>
+        <CookieConsent {...cookieData} onFormInteraction={onFormInteraction} />
+      </MemoryRouter>
     );
   });
+
   it('Renders correct content', () => {
     expect(
       screen.getByRole('link', { name: 'Privacy Policy' })
@@ -36,7 +54,6 @@ describe('Cookie consent form should render with correct content.', () => {
 
   it('Check correct cookie initialization', () => {
     const resultCookie = document.cookie;
-
     expect(resultCookie).toBe(
       `${COOKIE_CONSENT_KEY}={"responded":false,"answer":false}`
     );
