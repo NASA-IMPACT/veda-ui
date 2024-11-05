2682238f922917f517c1f5ae3b4995249221087d
Hanbyul Jo <hanbyul@nyu.edu>
2024-11-04 21:06:32 -0500

    Fix test

--- app/scripts/components/common/cookie-consent/cookieConsent.spec.js
+++ app/scripts/components/common/cookie-consent/cookieConsent.spec.js
@@ -4,14 +4,19 @@ import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom'; // For testing
 import { createMemoryHistory } from 'history';
-import * as utils from './utils';
 
+import * as utils from './utils';
 import CookieConsent from './index';
+const lodash = require('lodash');
 
 describe('Cookie consent form should render with correct content.', () => {
+  const setDisplayCookieConsent = jest.fn();
+  const setGoogleTagManager = jest.fn();
   const cookieData = {
     title: 'Cookie Consent',
-    copy: '<p>We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our <a href="https://www.nasa.gov/privacy/#cookies">Privacy Policy</a></p>We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our [Privacy Policy](https://www.nasa.gov/privacy/#cookies)'
+    copy: '<p>We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our <a href="https://www.nasa.gov/privacy/#cookies">Privacy Policy</a></p>We use cookies to enhance your browsing experience and to help us understand how our website is used. These cookies allow us to collect data on site usage and improve our services based on your interactions. To learn more about it, see our [Privacy Policy](https://www.nasa.gov/privacy/#cookies)',
+    setDisplayCookieConsent,
+    setGoogleTagManager
   };
 
   const onFormInteraction = jest.fn();
@@ -24,28 +29,14 @@ describe('Cookie consent form should render with correct content.', () => {
       pathname: 'localhost:3000/example/path'
     })
   }));
+
+  lodash.debounce = jest.fn((fn) => fn);
+
   afterEach(() => {
     jest.clearAllMocks();
-  });
-  it('Check that session item is non existant prior to cookie consent render. Then confirm that cookie consent creates session item.', () => {
-    expect(sessionStorage.getItem(utils.SESSION_KEY)).toBeNull();
-    render(
-      <MemoryRouter history={history}>
-        <CookieConsent {...cookieData} onFormInteraction={onFormInteraction} />
-      </MemoryRouter>
-    );
-    expect(sessionStorage.getItem(utils.SESSION_KEY)).toBe(`true`);
-  });
-  it('Check that getcookie is only called once on render and session item is true', () => {
-    const spy = jest.spyOn(utils, 'getCookie');
 
-    render(
-      <MemoryRouter history={history}>
-        <CookieConsent {...cookieData} onFormInteraction={onFormInteraction} />
-      </MemoryRouter>
-    );
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(sessionStorage.getItem(utils.SESSION_KEY)).toBe(`true`);
+    // Clear cookies after each test
+    document.cookie = `${utils.COOKIE_CONSENT_KEY}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
   });
 
   it('Renders correct content', () => {
@@ -91,8 +82,8 @@ describe('Cookie consent form should render with correct content.', () => {
         <CookieConsent {...cookieData} onFormInteraction={onFormInteraction} />
       </MemoryRouter>
     );
-    const button = screen.getByRole('button', { name: 'Accept Cookies' });
-    fireEvent.click(button);
+    const acceptButton = screen.getByRole('button', { name: 'Accept Cookies' });
+    fireEvent.click(acceptButton);
     const resultCookie = document.cookie;
 
     expect(resultCookie).toBe(
