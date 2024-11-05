117db83365ea72b5999f47c37ad1c0f2068f9930
Vitor George <vitor.george@gmail.com>
2024-11-05 12:31:40 +0000

    Data catalog filters default to closed

--- app/scripts/components/common/catalog/catalog-content.tsx
+++ app/scripts/components/common/catalog/catalog-content.tsx
@@ -214,7 +214,7 @@ function CatalogContent({
         allSelected={selectedFilters}
         exclusiveSourceSelected={exclusiveSourceSelected}
         customTopOffset={isSelectable ? 50 : 0}
-        openByDefault={isSelectable ? false : true}
+        openByDefault={false}
         pathname={pathname}
       />
       <Catalog>
