2c04768a24b6e6b769942f3b15272c900c8da51c
Alice Rühl <alice@developmentseed.org>
2024-11-07 10:32:05 +0100

    Format header file with prettier

--- app/scripts/components/common/page-header/index.tsx
+++ app/scripts/components/common/page-header/index.tsx
@@ -18,41 +18,61 @@ interface PageHeaderProps {
 }
 
 export default function PageHeader(props: PageHeaderProps) {
-  
   const [expanded, setExpanded] = useState(false);
-  const onClick = (): void => setExpanded(prvExpanded => !prvExpanded);
+  const onClick = (): void => setExpanded((prvExpanded) => !prvExpanded);
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  
-  const testMenuItems = [<a href="#linkOne" key="one">
+
+  const testMenuItems = [
+    <a href='#linkOne' key='one'>
       Simple link one
-    </a>, <a href="#linkTwo" key="two">
+    </a>,
+    <a href='#linkTwo' key='two'>
       Simple link two
-    </a>];
+    </a>
+  ];
 
-    const onToggle = () => setIsOpen(!isOpen);
+  const onToggle = () => setIsOpen(!isOpen);
 
-  const testItemsMenu = [<>
-      <USWDSNavDropDownButton onToggle={(): void => {
-      onToggle();
-    }} menuId="testDropDownOne" isOpen={isOpen[0]} label="Nav Label" isCurrent={true} />
-      <USWDSMenu key="one" items={testMenuItems} isOpen={isOpen[0]} id="testDropDownOne" />
-    </>, <a href="#two" key="two" className="usa-nav__link">
+  const testItemsMenu = [
+    <>
+      <USWDSNavDropDownButton
+        onToggle={(): void => {
+          onToggle();
+        }}
+        menuId='testDropDownOne'
+        isOpen={isOpen[0]}
+        label='Nav Label'
+        isCurrent={true}
+      />
+      <USWDSMenu
+        key='one'
+        items={testMenuItems}
+        isOpen={isOpen[0]}
+        id='testDropDownOne'
+      />
+    </>,
+    <a href='#two' key='two' className='usa-nav__link'>
       <span>Parent link</span>
-    </a>, <a href="#three" key="three" className="usa-nav__link">
+    </a>,
+    <a href='#three' key='three' className='usa-nav__link'>
       <span>Parent link</span>
-    </a>];
+    </a>
+  ];
 
-  return <>
+  return (
+    <>
       <USWDSHeader extended={true} showMobileOverlay={expanded}>
-        <div className="usa-navbar">
+        <div className='usa-navbar'>
           <USWDSHeaderTitle>Project Title</USWDSHeaderTitle>
-          <USWDSNavMenuButton onClick={onClick} label="Menu" />
+          <USWDSNavMenuButton onClick={onClick} label='Menu' />
         </div>
-        <USWDSExtendedNav primaryItems={testItemsMenu} secondaryItems={testMenuItems} mobileExpanded={expanded} onToggleMobileNav={onClick}>
-        </USWDSExtendedNav>
+        <USWDSExtendedNav
+          primaryItems={testItemsMenu}
+          secondaryItems={testMenuItems}
+          mobileExpanded={expanded}
+          onToggleMobileNav={onClick}
+        />
       </USWDSHeader>
-    </>;
+    </>
+  );
 }
-
-
-
