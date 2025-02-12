fb1b1eaa7b0f16994186c554a21dd507f6b31866
Hanbyul Jo <hanbyul@nyu.edu>
2025-02-12 09:46:02 -0500

    Cleanup

--- recommended-bump/index.mjs
+++ recommended-bump/index.mjs
@@ -4,30 +4,24 @@ import semver from 'semver';
 import { getSemverTags } from 'git-semver-tags';
 import createPreset from 'conventional-changelog-conventionalcommits';
 
-const preReleaseId = 'alpha';
 // Detect the version number based on conventional commits
 // This does not handle pre-release
 class RecommendedBump extends Plugin {
   async getIncrementedVersion(options) {
+    // loadPreset('conventional') shows different result
     const bumper = new Bumper(process.cwd());
     const conventionalCommitPreset = await createPreset();
-    bumper.commits(null, conventionalCommitPreset.parser);
-
     const recommendation = await bumper.bump(conventionalCommitPreset.whatBump);
 
     let { releaseType } = recommendation;
 
     const tags = await getSemverTags({
-      lernaTags: !!options.lernaPackage,
-      package: options.lernaPackage,
-      tagPrefix: options.tagPrefix,
-      skipUnstable: true,
-      cwd: options.cwd
+      skipUnstable: true
     });
 
     const lastStableTag = tags.length > 0 ? tags[0] : null;
     if (releaseType) {
-      return semver.inc(lastStableTag, releaseType, preReleaseId);
+      return semver.inc(lastStableTag, releaseType);
     }
   }
 
