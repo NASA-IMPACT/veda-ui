365a84b6b03959bf5005fa7ec8a613ba7d128c3e
Hanbyul Jo <hanbyul@nyu.edu>
2025-03-12 08:45:09 -0400

    Move findParentDataset function to useParentDataset hook

--- app/scripts/components/exploration/hooks/use-parent-data.ts
+++ app/scripts/components/exploration/hooks/use-parent-data.ts
@@ -1,8 +1,20 @@
 import { useState, useEffect } from 'react';
 import { useAtom } from 'jotai';
 import { externalDatasetsAtom } from '../atoms/datasetLayers';
-import { findParentDataset } from '$utils/data-utils';
 import { DatasetData } from '$types/veda';
+
+function findParentDataset({
+  datasets,
+  datasetId
+}: {
+  datasets: DatasetData[];
+  datasetId: string;
+}): DatasetData | undefined {
+  const parentDatset = datasets.find(
+    (dataset: DatasetData) => dataset.id === datasetId
+  );
+  return parentDatset;
+}
 
 export default function useParentDataset({ datasetId }: { datasetId: string }) {
   const [currentDataset, setCurrentDataset] = useState<DatasetData | undefined>(
--- app/scripts/utils/data-utils.ts
+++ app/scripts/utils/data-utils.ts
@@ -1,18 +1,5 @@
 import { VedaData, VedaDatum, DatasetData } from '$types/veda';
 
-export const findParentDataset = ({
-  datasets,
-  datasetId
-}: {
-  datasets: DatasetData[];
-  datasetId: string;
-}): DatasetData | undefined => {
-  const parentDatset = datasets.find(
-    (dataset: DatasetData) => dataset.id === datasetId
-  );
-  return parentDatset;
-};
-
 export const getDatasetLayers = (datasets: VedaData<DatasetData>) =>
   Object.values(datasets).flatMap((dataset: VedaDatum<DatasetData>) => {
     return dataset.data.layers;
