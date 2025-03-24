59b6763498cc6f27e2fc0c6474a27bc1ae0bcb93
Alice Rühl <alice@developmentseed.org>
2025-03-24 11:49:18 +0100

    Run prettier formatting on playwright specs

--- test/playwright/tests/about.spec.ts
+++ test/playwright/tests/about.spec.ts
@@ -4,19 +4,24 @@ test('about page should have no javascript errors', async ({
   page,
   aboutPage,
   consentBanner
- }) => {
+}) => {
   let pageErrorCalled = false;
   // Log all uncaught errors to the terminal
-  page.on('pageerror', exception => {
+  page.on('pageerror', (exception) => {
     console.warn(`Uncaught exception: "${exception}"`);
     pageErrorCalled = true;
   });
 
   await page.goto('/about');
   await consentBanner.acceptButton.click();
-  await expect(aboutPage.aboutParagraph, `learn page should load`).toBeVisible();
+  await expect(
+    aboutPage.aboutParagraph,
+    `learn page should load`
+  ).toBeVisible();
 
   // scroll page to bottom
   await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-  expect(pageErrorCalled, 'no javascript exceptions thrown on page').toBe(false)
+  expect(pageErrorCalled, 'no javascript exceptions thrown on page').toBe(
+    false
+  );
 });
--- test/playwright/tests/catalog.spec.ts
+++ test/playwright/tests/catalog.spec.ts
@@ -1,32 +1,42 @@
 import { test, expect } from '../pages/basePage';
 
-const catalogs = JSON.parse(require('fs').readFileSync('test/playwright/playwrightTestData.json', 'utf8'))['catalogs'];
+const catalogs = JSON.parse(
+  require('fs').readFileSync('test/playwright/playwrightTestData.json', 'utf8')
+)['catalogs'];
 
 test('catalogs displayed on /data-catalog route', async ({
   page,
   catalogPage,
-  consentBanner,
- }) => {
+  consentBanner
+}) => {
   let pageErrorCalled = false;
   // Log all uncaught errors to the terminal
-  page.on('pageerror', exception => {
+  page.on('pageerror', (exception) => {
     console.log(`Uncaught exception: "${exception}"`);
     pageErrorCalled = true;
   });
 
   await page.goto('/data-catalog');
   await consentBanner.acceptButton.click();
-  await expect(catalogPage.header, `catalog page should load`).toHaveText(/data catalog/i);
+  await expect(catalogPage.header, `catalog page should load`).toHaveText(
+    /data catalog/i
+  );
 
   for (const item of catalogs) {
-    await test.step(`locate ${item} catalog card`, async() => {
-      const catalogCard =  catalogPage.mainContent.getByRole('article').getByRole('heading', { level: 3, name: item, exact: true}).last();
+    await test.step(`locate ${item} catalog card`, async () => {
+      const catalogCard = catalogPage.mainContent
+        .getByRole('article')
+        .getByRole('heading', { level: 3, name: item, exact: true })
+        .last();
       await catalogCard.scrollIntoViewIfNeeded();
-      await expect(catalogCard, `${item} catalog card should load`).toBeVisible();
-    })
-    
-  };
+      await expect(
+        catalogCard,
+        `${item} catalog card should load`
+      ).toBeVisible();
+    });
+  }
 
-  expect(pageErrorCalled, 'no javascript exceptions thrown on page').toBe(false)
-
+  expect(pageErrorCalled, 'no javascript exceptions thrown on page').toBe(
+    false
+  );
 });
