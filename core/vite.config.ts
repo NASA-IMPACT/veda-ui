86a1dd5ab8b12ba730b02f93c21f7aa8cc081fe0
Alice Rühl <alice@developmentseed.org>
2025-04-07 13:25:40 +0200

    Add simple story for USWDS button

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      $uswds: path.resolve(__dirname, '../app/scripts/uswds')
+    }
+  }
 });
