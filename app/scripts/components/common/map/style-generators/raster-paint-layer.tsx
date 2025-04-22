1d05740146efcf8f4e1ce08e64c92ce2bcf87b77
Hanbyul Jo <hanbyul@nyu.edu>
2025-04-22 17:00:41 +0900

    Add more parameterse to repeat

--- app/scripts/components/common/map/style-generators/raster-paint-layer.tsx
+++ app/scripts/components/common/map/style-generators/raster-paint-layer.tsx
@@ -30,13 +30,15 @@ interface RasterPaintLayerProps extends BaseGeneratorParams {
 }
 
 export function formatTitilerParameter(params): string {
-  // bands and assets need to be sent as repeat query params
-  const { bands, bidx, assets, bbox, ...regularParams } = params;
+  // array<any> parameter except the one comma delimiated (Ex.rescale)
+  // https://openveda.cloud/api/raster/docs#/STAC%20Search/tilejson_searches__search_id___tileMatrixSetId__tilejson_json_get
+  const { bands, bidx, asset_bidx, assets, bbox, ...regularParams } = params;
 
   const repeatParams: Record<string, string[] | undefined> = {
     bands,
     assets,
-    bidx
+    bidx,
+    asset_bidx
   };
 
   const regularParamsString = qs.stringify(regularParams, {
