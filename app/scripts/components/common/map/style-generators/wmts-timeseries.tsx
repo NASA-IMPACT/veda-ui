2e8926e6bec9bb0f41a3f808841b67dcc5c925c4
Ram Sharan Rimal <rsr0012@uah.edu>
2025-04-30 10:31:21 -0500

    Add wmts component

---
+++ app/scripts/components/common/map/style-generators/wmts-timeseries.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import useFitBbox from '../hooks/use-fit-bbox';
+import { BaseGeneratorParams } from '../types';
+import { RasterPaintLayer } from './raster-paint-layer';
+
+import {
+  useRequestStatus,
+  useWMTS
+} from '$components/common/map/style-generators/hooks';
+import { ActionStatus } from '$utils/status';
+
+import { userTzDate2utcString } from '$utils/date';
+
+export interface MapLayerWMTSProps extends BaseGeneratorParams {
+  id: string;
+  date?: Date;
+  stacCol: string;
+  sourceParams?: Record<string, any>;
+  stacApiEndpoint?: string;
+  zoomExtent?: number[];
+  onStatusChange?: (result: { status: ActionStatus; id: string }) => void;
+}
+
+export function WMTSTimeseries(props: MapLayerWMTSProps) {
+  const { id, stacCol, date, stacApiEndpoint, onStatusChange } = props;
+  const stacApiEndpointToUse = stacApiEndpoint ?? process.env.API_STAC_ENDPOINT;
+  const { wmtsUrl, bounds } = useWMTS({
+    id,
+    stacCol,
+    stacApiEndpointToUse,
+    onStatusChange
+  });
+
+  const { changeStatus } = useRequestStatus({
+    id,
+    onStatusChange,
+    requestsToTrack: []
+  });
+  const tileParams = {
+    format: 'image/png',
+    Service: 'WMTS',
+    Request: 'GetTile',
+    Version: '1.0.0',
+    layer: stacCol,
+    style: 'default',
+    tilematrixset: '2km',
+    ...(date && { TIME: userTzDate2utcString(date) })
+  };
+
+  useFitBbox(false, undefined, bounds);
+
+  return (
+    <RasterPaintLayer
+      {...props}
+      tileApiEndpoint={wmtsUrl}
+      tileParams={tileParams}
+      generatorPrefix='wmts'
+      onStatusChange={changeStatus}
+      metadataFormatter={(_tileJsonData, tileParamsAsString) => {
+        // console.log({ _tileJsonData, tileParamsAsString });
+        return { wmsTileUrl: `${wmtsUrl}?${tileParamsAsString}` };
+      }}
+      sourceParamFormatter={(url) => {
+        const tileUrl = `${url}&TileCol={x}&TileRow={y}&TileMatrix={z}`;
+        return {
+          tiles: [tileUrl],
+          tileSize: 256
+        };
+      }}
+    />
+  );
+}
