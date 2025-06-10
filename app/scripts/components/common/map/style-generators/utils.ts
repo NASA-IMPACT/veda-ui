05e4957c921ccbb10b51fb52e24aadb1cd062912
Aimee Barciauskas <aimee@developmentseed.org>
2025-06-10 14:02:56 -0700

    working on it

--- app/scripts/components/common/map/style-generators/utils.ts
+++ app/scripts/components/common/map/style-generators/utils.ts
@@ -2,7 +2,8 @@ import qs from 'qs';
 export function formatTitilerParameter(params): string {
   // array<any> parameter except the one comma delimiated (Ex.rescale)
   // https://openveda.cloud/api/raster/docs#/STAC%20Search/tilejson_searches__search_id___tileMatrixSetId__tilejson_json_get
-  const { bands, bidx, asset_bidx, assets, bbox, sel, ...regularParams } = params;
+  const { bands, bidx, asset_bidx, assets, bbox, sel, ...regularParams } =
+    params;
 
   const repeatParams: Record<string, string[] | undefined> = {
     bands,
