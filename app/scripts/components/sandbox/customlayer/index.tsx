723ff07f186a14221d2967b710d528574751a235
Hanbyul Jo <hanbyul@nyu.edu>
2025-07-03 12:44:18 -0400

    Develop custom layer exmaple a bit more

--- app/scripts/components/sandbox/customlayer/index.tsx
+++ app/scripts/components/sandbox/customlayer/index.tsx
@@ -14,29 +14,8 @@ import { veda_faux_module_datasets } from '$data-layer/datasets';
 import useMapStyle from '$components/common/map/hooks/use-map-style';
 import useGeneratorParams from '$components/common/map/hooks/use-generator-params';
 
-const sampleGeojson = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        coordinates: [
-          [
-            [-84.4284851853547, 33.78207383698731],
-            [-84.4284851853547, 33.729798445954984],
-            [-84.34451965871028, 33.729798445954984],
-            [-84.34451965871028, 33.78207383698731],
-            [-84.4284851853547, 33.78207383698731]
-          ]
-        ],
-        type: 'Polygon'
-      }
-    }
-  ]
-};
 export function GeoJSONLayer(props) {
-  const { id, geojsonData } = props;
+  const { id, geojsonURL, styleOverrides } = props;
 
   const { updateStyle } = useMapStyle();
 
@@ -48,7 +27,7 @@ export function GeoJSONLayer(props) {
     const sources: Record<string, SourceSpecification> = {
       [id]: {
         type: 'geojson',
-        data: geojsonData
+        data: geojsonURL
       } as GeoJSONSourceSpecification
     };
 
@@ -56,11 +35,25 @@ export function GeoJSONLayer(props) {
       // Line background layer
       {
         id: `${id}-line-bg`,
-        type: 'line',
         source: id,
+        ...styleOverrides,
+        metadata: {
+          layerOrderPosition: 'markers'
+        }
+      },
+      {
+        id: `${id}-line-text`,
+        source: id,
+        type: 'symbol',
+        layout: {
+          'symbol-placement': 'line',
+          'text-field': 'Custom area',
+          'text-size': 12
+        },
         paint: {
-          'line-color': '#f00',
-          'line-width': 7
+          'text-color': '#222',
+          'text-halo-color': '#fff',
+          'text-halo-width': 3
         },
         metadata: {
           layerOrderPosition: 'markers'
@@ -74,7 +67,7 @@ export function GeoJSONLayer(props) {
       layers,
       params: generatorParams
     });
-  }, [geojsonData, id, generatorId, generatorParams, updateStyle]);
+  }, [geojsonURL, id, generatorId, generatorParams, updateStyle]);
 
   //
   // Cleanup layers on unmount
@@ -103,16 +96,19 @@ export default function CustomLayerDemo() {
               layerId='no2-monthly'
               datasets={veda_faux_module_datasets}
               center={[-84.39, 33.75]}
-              zoom={9.5}
+              zoom={5.5}
               dateTime='2019-06-01'
             >
               <GeoJSONLayer
                 id='geojson-id'
-                geojsonData={sampleGeojson}
+                geojsonURL='/public/geo-data/states/Georgia.geojson'
                 styleOverrides={{
-                  lineColor: '#ff4757',
-                  fillColor: '#3742fa',
-                  fillOpacity: 0.6
+                  type: 'line',
+                  paint: {
+                    'line-color': '#eee',
+                    'line-dasharray': [3, 1],
+                    'line-width': 3
+                  }
                 }}
               />
             </BlockMap>
