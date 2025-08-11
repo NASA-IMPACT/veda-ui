3b46a3d083cd54eec8904749aee8cf2b624d70f3
Vitor George <vitor.george@gmail.com>
2025-08-11 16:26:51 +0100

    feat: upgrade Storybook to v9

--- core/.storybook/main.ts
+++ core/.storybook/main.ts
@@ -1,16 +1,23 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import path from 'path';
+
+const uswdsPath = path.resolve(__dirname, '../../app/scripts/uswds');
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@chromatic-com/storybook',
-    '@storybook/experimental-addon-test'
-  ],
+  addons: ['@storybook/addon-onboarding', '@storybook/addon-vitest'],
   framework: {
     name: '@storybook/react-vite',
     options: {}
+  },
+  viteFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        $uswds: uswdsPath
+      };
+    }
+    return config;
   }
 };
 export default config;
