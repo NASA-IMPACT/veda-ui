608042f21c50628590f115e3610fd5968d796bf4
Vitor George <vitor.george@gmail.com>
2025-08-11 17:44:03 +0100

    fix: import USWDS in storybook, setup styles

--- core/.storybook/preview.ts
+++ core/.storybook/preview.ts
@@ -1,5 +1,8 @@
 import type { Preview } from '@storybook/react';
 
+// Import USWDS core styles globally for all stories
+import '@uswds/uswds/css/uswds.css';
+
 const preview: Preview = {
   parameters: {
     controls: {
