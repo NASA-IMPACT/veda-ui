7b90b10759a418a8d2bf896b4286a20ab9dec54a
Hanbyul Jo <hanbyul@nyu.edu>
2025-08-11 13:30:21 -0400

    Expose timeline related hooks and layercardwithsync

--- app/scripts/libs/hooks.ts
+++ app/scripts/libs/hooks.ts
@@ -3,3 +3,11 @@ export { VedaUIProvider } from '$context/veda-ui-provider';
 export { default as ReactQueryProvider } from '$context/react-query';
 export { default as useTimelineDatasetAtom } from '$components/exploration/hooks/use-timeline-dataset-atom';
 export { useFiltersWithQS } from '$components/common/catalog/controls/hooks/use-filters-with-query';
+
+// E&A
+export {
+  useTimelineDatasetSettings,
+  useTimelineDatasetVisibility,
+  useTimelineDatasetColormap,
+  useTimelineDatasetColormapScale
+} from '$components/exploration/atoms/hooks';
