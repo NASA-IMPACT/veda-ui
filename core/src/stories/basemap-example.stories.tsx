c0cb92f859439e5481102199c93eb4c0541a4efb
Vitor George <vitor.george@gmail.com>
2025-08-14 10:50:32 +0100

    feat: add basic controls

--- core/src/stories/basemap-example.stories.tsx
+++ core/src/stories/basemap-example.stories.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { StorybookProviders } from '../storybook-providers';
-import Map from '$components/common/map';
+import Map, { MapControls } from '$components/common/map';
+import {
+  NavigationControl,
+  ScaleControl
+} from '$components/common/map/controls';
+
 import { Basemap } from '$components/common/map/style-generators/basemap';
 import { useBasemap } from '$components/common/map/controls/hooks/use-basemap';
+import MapCoordsControl from '$components/common/map/controls/coords';
 
 const BaseMapExample: React.FC = () => {
   const { mapBasemapId, labelsOption, boundariesOption } = useBasemap();
@@ -23,6 +29,11 @@ const BaseMapExample: React.FC = () => {
               labelsOption={labelsOption}
               boundariesOption={boundariesOption}
             />
+            <MapControls>
+              <NavigationControl position='top-right' />
+              <ScaleControl />
+              <MapCoordsControl />
+            </MapControls>
           </Map>
         </StorybookProviders>
       </div>
