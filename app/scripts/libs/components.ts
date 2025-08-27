22aeb8c0ba5c146c655be8538a8cce98262a7187
Hanbyul Jo <hanbyul@nyu.edu>
2025-08-27 14:40:52 -0400

    Export the components

---
+++ app/scripts/libs/components.ts
@@ -0,0 +1,3 @@
+export { ColormapSection } from '$components/exploration/components/datasets/colormap-section';
+export { default as LayerMenuOptions } from '$components/exploration/components/datasets/layer-options-menu';
+export { default as DatasetLayerCard } from '$components/exploration/components/datasets/data-layer-card-presentational';
