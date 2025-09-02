9f3b54a8aae82cf22715a5dccbc98f7c3780e7e3
Sandra Hoang <sandrahoang686@gmail.com>
2025-09-02 09:26:37 -0400

    initial timeline analysis setup in sandbox

---
+++ app/scripts/components/sandbox/legacy/exploration/timeline-analysis/index.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { useSetAtom } from 'jotai';
+import { GridContainer, Grid } from '@trussworks/react-uswds';
+import {
+  mockDatasets,
+  mockRawData,
+  mockSelectedDay,
+  mockSelectedCompareDay,
+  mockSetDatasets
+} from '../mock-data';
+import ExternalLayerCardExample from './external-layer-card';
+import { PageMainContent } from '$styles/page';
+import { ExplorationMap } from '$components/exploration/components/map';
+import { externalDatasetsAtom } from '$components/exploration/atoms/datasetLayers';
+import useTimelineDatasetAtom from '$components/exploration/hooks/use-timeline-dataset-atom';
+
+export const HugResetter = styled.div`
+  /* To escape from HUG grid */
+`;
+
+const DemoExplorationMap = styled(ExplorationMap)`
+  height: 40rem;
+`;
+
+const SandboxTimelineAnalysis = () => {
+  const setEAExternalDatasets = useSetAtom(externalDatasetsAtom);
+  setEAExternalDatasets(mockRawData);
+
+  const [timelineDatasets, setTimelineDatasets] = useTimelineDatasetAtom();
+
+  useEffect(() => {
+    // IF no data came through URL
+    if (timelineDatasets.length == 0) {
+      setTimelineDatasets([...mockDatasets]);
+    }
+  }, [timelineDatasets, setTimelineDatasets]);
+
+  return (
+    <PageMainContent>
+      <HugResetter>
+        <GridContainer id='grid-contianer'>
+          <Grid row gap={3}>
+            <Grid style={{ height: '300px' }} col={8}>
+              <DemoExplorationMap
+                datasets={timelineDatasets}
+                setDatasets={setTimelineDatasets}
+                selectedDay={mockSelectedDay}
+                selectedCompareDay={mockSelectedCompareDay}
+              />
+            </Grid>
+            <Grid col={12}>
+              <h4>External Layer Cards</h4>
+              {timelineDatasets &&
+                timelineDatasets.map((dataset) => (
+                  <ExternalLayerCardExample
+                    key={dataset.data.id}
+                    dataset={dataset}
+                    selectedDay={mockSelectedDay}
+                    setLayerInfo={() => {}}
+                  />
+                ))}
+            </Grid>
+          </Grid>
+        </GridContainer>
+      </HugResetter>
+    </PageMainContent>
+  );
+};
+
+export default SandboxTimelineAnalysis;
