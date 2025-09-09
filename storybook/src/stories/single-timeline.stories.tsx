7d4dff8eb46b1103eec4082f2b898b4ae02abddb
Sandra Hoang <sandrahoang686@gmail.com>
2025-09-09 09:42:32 -0400

    add SingleTimeline to storybook

---
+++ storybook/src/stories/single-timeline.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { mockDatasetAnalysisSuccess } from './mock-dataset-analysis-success';
+import TimelineSingle from '$components/exploration/components/timeline-single/timeline';
+import { useScales } from '$components/exploration/hooks/scales-hooks';
+
+const SingleTimelineExample = () => {
+  const { scaled: xScaled, main: xMain } = useScales();
+  return (
+    <>
+      <TimelineSingle
+        dataset={mockDatasetAnalysisSuccess}
+        selectedDay={new Date('2017-12-01T00:00:00.000Z')}
+        setSelectedDay={() => true}
+        selectedCompareDay={null}
+        setSelectedCompareDay={() => true}
+        panelHeight={100}
+        selectedInterval={{
+          start: new Date('2017-12-01T00:00:00.000Z'),
+          end: new Date('2018-12-01T00:00:00.000Z')
+        }}
+        setSelectedInterval={() => true}
+        dataDomain={[
+          new Date('2017-12-01T00:00:00.000Z'),
+          new Date('2018-12-01T00:00:00.000Z')
+        ]}
+        setObsolete={() => true}
+        scaleFactors={{ k0: 1, k1: 1 }}
+        xScaled={xScaled}
+        xMain={xMain}
+      />
+    </>
+  );
+};
+
+const meta: Meta<typeof SingleTimelineExample> = {
+  title: 'Components/SingleTimelineExample',
+  component: SingleTimelineExample,
+  parameters: {
+    layout: 'fullscreen'
+  },
+  tags: ['autodocs']
+};
+
+export default meta;
+type Story = StoryObj<typeof SingleTimelineExample>;
+
+export const Default: Story = {
+  render: () => <SingleTimelineExample />
+};
