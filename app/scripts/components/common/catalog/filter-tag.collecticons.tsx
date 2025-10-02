cd6600399de292a7f21c22f23be43d964d841416
Vitor George <vitor.george@gmail.com>
2025-10-02 11:53:00 +0100

    feat: add filter tag comparison story and Collecticons baseline

---
+++ app/scripts/components/common/catalog/filter-tag.collecticons.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { CollecticonXmarkSmall } from '@devseed-ui/collecticons';
+
+import { USWDSButton, USWDSTag } from '$uswds';
+import { OptionItem } from '$components/common/form/checkable-filter';
+
+interface FilterTagProps {
+  item: OptionItem;
+  onClick: (item: OptionItem) => void;
+}
+
+export default function FilterTagCollecticons(props: FilterTagProps) {
+  const { item, onClick } = props;
+
+  const handleClick = () => {
+    onClick(item);
+  };
+
+  return (
+    <USWDSTag className='border-1px border-base-light tablet:padding-y-05 tablet:padding-x-1 padding-y-0 padding-x-05 tablet:font-sans-2xs font-sans-3xs display-flex flex-align-center text-normal text-base-darkest bg-white text-no-uppercase'>
+      {item.name}
+      <USWDSButton
+        type='button'
+        onClick={handleClick}
+        unstyled
+        className='text-base tablet:margin-y-2px margin-left-2px margin-y-0'
+      >
+        <CollecticonXmarkSmall aria-label={`Remove ${item.name} filter`} />
+      </USWDSButton>
+    </USWDSTag>
+  );
+}
