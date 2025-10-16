bdae570ff770b16d03e7a3b3641fd4866525b5b5
Vitor George <vitor.george@gmail.com>
2025-10-16 14:53:53 +0100

    feat: complete collecticons dependency removal
    Remove @devseed-ui/collecticons from package.json, delete stream shim from storybook config, and remove icons-legacy/ directory. All collecticons functionality migrated to USWDS icons and custom USWDS-style icons.

--- storybook/utils/stream-shim.js
+++
@@ -1,5 +0,0 @@
-// Shim for @devseed-ui/collecticons compatibility
-// The library uses 'react-dom/server', which is not supported in Vite
-// See: https://github.com/developmentseed/ui-library-seed/issues/237
-export class Readable {}
-export default { Readable };
